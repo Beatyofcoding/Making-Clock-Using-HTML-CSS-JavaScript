d0eeac74ab3a9b09980c6c08e5a30ceac13904a9
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-04-25 23:23:34 +0200

    Creating JavaScript File

---
+++ script.js
@@ -0,0 +1,5 @@
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[date-hour-hand]')
+const minuteHand = document.querySelector('[date-minute-hand]')
+const secondHand = document.querySelector('[date-second-hand]')
