cf6e0621a6d15f08b1757a0fe4f093167c863996
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-04-25 23:55:57 +0200

    Declaring the variables on Javascript file

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ setInterval(setClock, 1000)
 const hourHand = document.querySelector('[date-hour-hand]')
 const minuteHand = document.querySelector('[date-minute-hand]')
 const secondHand = document.querySelector('[date-second-hand]')
+
